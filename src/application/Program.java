ed9f80f92009fae0fcc42481f4e4ac0a4b29d1e3
Danilo Souza <daniloandresouza@gmail.com>
2020-12-30 15:09:44 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,23 @@
 package application;
 
-import java.sql.Connection;
+import java.util.Date;
 
-import db.DB;
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+		/*
 		Connection conn = DB.getConnection();
-		
 		DB.closeConnection();
-
+		 */
+		
+		Department department = new Department(1, "Books");
+		
+		Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, department);
+		
+		System.out.println(seller);
 	}
 
 }
